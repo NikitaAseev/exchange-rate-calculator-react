dd7ec8a4dcf2c34fd56f383388f9071a00941305
Nikita <nikita@Ivans-MacBook-Pro.local>
2020-03-19 11:58:29 +0200

    Updates

---
+++ precache-manifest.074244d8812b6227a4b72d4d951b2e7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f653b0718fc78ff50175568188cf010d",
+    "url": "/exchange-rate-calculator-react/index.html"
+  },
+  {
+    "revision": "f518f05550378e00408e",
+    "url": "/exchange-rate-calculator-react/static/css/main.562f7fab.chunk.css"
+  },
+  {
+    "revision": "f586224a8cd85aa93e5c",
+    "url": "/exchange-rate-calculator-react/static/js/2.a3d7ca3f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/exchange-rate-calculator-react/static/js/2.a3d7ca3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f518f05550378e00408e",
+    "url": "/exchange-rate-calculator-react/static/js/main.381df5eb.chunk.js"
+  },
+  {
+    "revision": "38ed62de2bc068bf06c0",
+    "url": "/exchange-rate-calculator-react/static/js/runtime-main.ab59dd28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/exchange-rate-calculator-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
